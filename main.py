f9ea95b2de627ed362529bf00ab4c4361530a30c
joshuabenavidesV <josbenavides358@gmail.com>
2025-07-18 13:31:09 -0500

    worked with team to get post route to work and created 2 functions for the load datA and save route

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
 import json
+from pydantic import BaseModel
+
 app = FastAPI()
 
-seed_Data = "seedData.json"
-with open(seed_Data, "r") as file:
-    data = json.load(file)
-    
+def load_data():
+    seed_Data = "seedData.json"
+    with open(seed_Data, "r") as file:
+        data = json.load(file)
+    return data
+
+def save_data():
+    save_data = "data.json"
+    with open(save_data, "w") as file:
+        json.dump(data, file)
+
+data = load_data()
+
+class Snippet(BaseModel):
+    language: str
+    code: str
+
 @app.get("/snippets")
 def get_Allsnippets():
     return data
@@ -16,6 +31,18 @@ def get_snippets_id(snippet_id: int):
         if snippet.get("id") == snippet_id:
             return snippet
     return {"error": "Snippet not found"}
+
+@app.post("/snippets")
+def create_snippet(snippet: Snippet):
+    new_snippet = {
+        "id": len(data) + 1,
+        "language": snippet.language,
+        "code": snippet.code,
+    }
+    data.append(new_snippet)
+    save_data()
+    return new_snippet
+    
 
 
 
